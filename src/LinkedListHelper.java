c531fd6d1e9b6282d0bb8d855353556f2de220e3
Vatsal-Bajpai <vatsal.bajpai05@gmail.com>
2016-08-04 14:44:26 +0530

    Initial commit

---
+++ src/LinkedListHelper.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Vatsal on 04/08/16.
+ */
+public class LinkedListHelper {
+
+    public static class ListNode {
+
+        int data;
+        public ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+}
---
+++ src/MainClass.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Vatsal on 04/08/16.
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
