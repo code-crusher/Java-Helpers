1034dc68cba2746143a5eab34b15748f522767c6
Vatsal-Bajpai <vatsal.bajpai05@gmail.com>
2016-08-04 14:56:42 +0530

    [+] Added listnode head

--- src/LinkedListHelper.java
+++ src/LinkedListHelper.java
@@ -1,14 +1,16 @@
 /**
  * Created by Vatsal on 04/08/16.
  */
-public class LinkedListHelper {
+class LinkedListHelper {
 
-    public static class ListNode {
+    public ListNode head;
+
+    static class ListNode {
 
         int data;
         public ListNode next;
 
-        public ListNode(int data) {
+        ListNode(int data) {
             this.data = data;
         }
     }
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,13 @@
 /**
  * Created by Vatsal on 04/08/16.
  */
+
 public class MainClass {
 
     public static void main(String[] args) {
+
+        LinkedListHelper.ListNode listNode = new LinkedListHelper.ListNode(0);
+
 
 
     }
