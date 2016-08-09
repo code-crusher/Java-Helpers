c8e706f848b244fab90c26e496ab042b68c423f9
Vatsal-Bajpai <vatsal.bajpai05@gmail.com>
2016-08-09 23:58:48 +0530

    feat: added TreeNode class feat: implemented BinaryTree

--- src/trees/BinaryTreeHelper.java
+++ src/trees/BinaryTreeHelper.java
@@ -7,6 +7,24 @@ package trees;
 // Helper class to manipulate Simple Binary Trees
 public class BinaryTreeHelper {
 
+    public TreeNode root;
 
+    public BinaryTreeHelper(int data) {
+        root.data = data;
+    }
+
+    public BinaryTreeHelper() {
+        root = null;
+    }
+
+    // add left of the node
+    public void addLeft(TreeNode node, int data) {
+        node.left = new TreeNode(data);
+    }
+
+    // add right of the node
+    public void addRight(TreeNode node, int data) {
+        node.right = new TreeNode(data);
+    }
 
 }
---
+++ src/trees/TreeNode.java
@@ -0,0 +1,16 @@
+package trees;
+
+/**
+ * Created by Vatsal on 09/08/16.
+ */
+public class TreeNode {
+
+    public int data;
+    public TreeNode left, right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
